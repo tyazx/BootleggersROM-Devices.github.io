af753eb8f50746ca80d0a347fbbbd8a0c6c9eb76
amog787 <amoghmaiya@gmail.com>
2019-03-16 22:52:46 +0530

    kenzo: CHangelog

---
+++ _changelog/kenzo.mk
@@ -0,0 +1,26 @@
+---
+title:kenzo
+layout:changelayout 
+---
+
+==============
+   16/03/19
+==============
+
+Device
+
+- Amol's tree
+- Advanced Controls
+- Kenzo Sax
+- Stock Thermals
+- Misc stuff
+
+Bootleggers
+
+- Quick settings header images
+- Ambient visualiser
+- Lockscreen clocks
+- Misc under-the-hood changes
+- March Sec Patch
+
+CLEAN FLASH REQUIRED
