6e32f825088b36a0a13d68d33700381fbd985d57
Fullbustah <adam.melicharek@gmail.com>
2019-03-24 16:51:13 +0100

    Update kuntao

---
+++ _changelog/kuntao.mk
@@ -0,0 +1,13 @@
+---
+title:kuntao
+layout:changelayout 
+---
+
+==============
+   24/03/19
+==============
+
+* Add support for exfat and NTFS
+* Add wireless display
+* Upstream kernel to v3.18.137
+* Other fixes by Atridxx
