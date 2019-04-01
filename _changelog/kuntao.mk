472885f2dd49995895611aaa81c49988375e3e2c
Fullbustah <adam.melicharek@gmail.com>
2019-04-01 17:44:27 +0200

    Update kuntao.mk

--- _changelog/kuntao.mk
+++ _changelog/kuntao.mk
@@ -4,10 +4,32 @@ layout:changelayout
 ---
 
 ==============
-   24/03/19
+   01/04/19
 ==============
+Bootleggers 4.1
 
-* Add support for exfat and NTFS
-* Add wireless display
-* Upstream kernel to v3.18.137
-* Other fixes by Atridxx
+* ROM rebase to fix statusbar clock, now it should be work correctly, and even behave nicely on notch devices!
+* Statusbar Headers 
+* Lockscreen clocks
+* Launcher3 rebased and completely up-to-date
+* Switch volume panel position + Extended (like Oreo)
+* Grid recents are back
+* Recents clear all + membar
+* Ambient ticker with new UI (based on Q beta)
+* Minimal stuff, like FP vibration authentication, disable qs slide on secured phones, tiles like sync and ambient display
+* Switched to gboard go and phonograph
+* Slim Recents
+* Added changelog section
+* Themes: Themed more apps like OTA, Dialer, Contacts, File manager and phonograph
+* OP gestures
+* Smart and quick pulldown
+* Some more optimization to Ambient Ticker and many other features
+* Added john notwoodman, who isn't woodman
+
+Device changes
+
+* Add Doze 
+* Fix sepolicy denials
+* Upgrade kernel to 4.9
+* Add EAS and some other spicy stuff
+* Add GCam
