c821d7a9eb729b8e30378f2e2294448fef40878e
Sven <tribual@web.de>
2019-11-14 09:58:31 +0100

    Delete cepheus.mk

--- _devicesbeta/cepheus.mk
+++
@@ -1,9 +0,0 @@
----
-codename: cepheus
-fullname: Xiaomi Mi 9
-maintainer: tribual
-filename: BootleggersROM-Queue4cepheus.4.7-MadStinky-Shishufied-20191111-100634.zip
-newformat: true
-notes: Initial build, Selinux permissive
-buildsize: 861152136
----
